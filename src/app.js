94f0a6191f2dd462e8a369e4cb998123005c0dce
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2024-09-14 18:31:39 +0200

    Log in Function
    Created Login in function that was successfully checked with my credentials

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import './css/style.css';
 
 import router from './js/router';
-
+import { login } from './js/api/auth/login';
 await router(window.location.pathname);
 console.log('hello world');
--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,37 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from '../constants';
+export async function login({ email, password }) {
+  try {
+    const response = fetch(API_AUTH_LOGIN, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    });
+    if (!response) {
+      throw new Error('error.message');
+    } else {
+      console.log('Successful log in');
+    }
+    const data = (await response).json();
+    return data;
+  } catch (error) {
+    console.error('Login error:', error.message);
+    throw error;
+  }
+}
+
+const data = {
+  name: 'angZar',
+  email: 'first.last@stud.noroff.no',
+  password: 'UzI1NiIsInR5cCI',
+};
+const data1 = {
+  email: 'angzar49347@stud.noroff.no',
+  password: '/.,offThe12qw3p09/',
+};
+
+login(data1);
